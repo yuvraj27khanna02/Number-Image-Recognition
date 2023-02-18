1757c5c72ba85e20b519d3a464a63b62349e59d9
Yuvraj Khanna <you@example.com>
2023-02-18 05:01:10 -0500

    added model_functions with common model methods

---
+++ model_functions.py
@@ -0,0 +1,46 @@
+import tensorflow as tf
+
+def create_model(activation_function: str):
+    model = tf.keras.Sequential([
+        tf.keras.layers.Conv2D(256, (3,3), activation=str(activation_function), input_shape=(28, 28, 1)),
+        tf.keras.layers.MaxPool2D(pool_size=(2,2)),
+        tf.keras.layers.Conv2D(256, (3,3), activation=str(activation_function), input_shape=(28, 28, 1)),
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(256, activation=str(activation_function)),
+        tf.keras.layers.Dense(10, activation="softmax")
+    ])
+    model.compile(
+    optimizer=tf.keras.optimizers.Adam(), 
+    loss=tf.keras.losses.SparseCategoricalCrossentropy(),
+    metrics=["accuracy"]
+    )
+    return model
+
+def all_act_fns() -> list:
+    return ["linear", "sigmoid", "hard_sigmoid", "tanh", "softmax",
+            "relu", "leaky_selu", "elu", "selu", "swish"]
+
+def CNN_model():
+    NUMBER_OF_OUTPUTS = 10
+
+    MNIST_model = tf.keras.Sequential([
+
+        tf.keras.layers.Conv2D(32, (3, 3), activation="relu", input_shape=(28, 28, 1)),
+        tf.keras.layers.MaxPool2D(pool_size=2, strides=2),
+
+        tf.keras.layers.Conv2D(64, (3, 3), activation="relu"),
+        tf.keras.layers.MaxPool2D(pool_size=2, strides=2),
+
+        tf.keras.layers.Conv2D(128, (3, 3), activation="sigmoid"),
+        tf.keras.layers.MaxPool2D(pool_size=(2, 2)),
+
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(1024, activation="relu"),
+        tf.keras.layers.Dense(NUMBER_OF_OUTPUTS, activation="softmax")
+    ])
+
+    MNIST_model.compile(
+        optimizer=tf.keras.optimizers.Adam(), 
+        loss=tf.keras.losses.SparseCategoricalCrossentropy(),
+        metrics=["accuracy"]
+    )
