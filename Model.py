24476ba066570c0f2c06b42519d4e0ea6b9ceffb
Yuvraj Khanna <68678325+yuvraj27khanna02@users.noreply.github.com>
2023-02-21 09:06:20 -0500

    Update Model.py
    Updated model to report best model based on parameters and syntax in model_functions.py

--- Model.py
+++ Model.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 import numpy as np
 from matplotlib import pyplot as plt
+from model_functions import *
 
 (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
 
@@ -24,13 +25,13 @@ x_test_bin = np.array(x_test > 0.5, dtype=np.float32)
 
 #   model testing
 
-from model_functions import create_model, all_act_fns
-
 print("                             ------------------------------------")
 
-for act_fn in all_act_fns():
-    print(f"               <-- <-- <-- {act_fn} --> --> -->    ")
-    model = create_model(str(act_fn))
-    model.fit(x=x_train_bin, y=y_train, 
-              epochs=1, verbose=2)
-    print("                             ###################################")
+for k_i_fn in all_k_i_fns():
+    for act_fn in all_act_fns():
+        for opt_fn in all_opt_fns():
+            for loss_fn in all_loss_fns():
+                model = create_basic_model(k_i_fn, act_fn, opt_fn, loss_fn)
+                model.fit(x=x_train_bin, y=y_train, epochs=10, verbose=2)
+                print(f"	Kernel initialiser: {k_i_fn} ; Activation function: {act_fn} ; Optimizer function: {opt_fn} ; Loss function: {loss_fn}")
+                print("                             ###################################")
